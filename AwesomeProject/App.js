9fff4854ee994792f021b8b9441696f511b78f81
林采如 <lintsaiju@lincairudeMacBook-Pro.local>
2022-09-23 14:31:21 -0700

    w2

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput} from 'react-native';
+import Header from './components/Header.js';
+import React,{useState} from 'react';
+import Input from './components/Input.js';
 
 export default function App() {
+const onTextAdd=function(newText){
+  console.log(newText);
+}
+
   const name="my app";
+  const [text,setText]=useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on {name}!</Text>
+      {/* <Text>Open up App.js to start working on {name}!</Text> */}
+      <Header appName={name}/>
+      <Input onAdd={onTextAdd}/>
+      <Text>inputed:{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
