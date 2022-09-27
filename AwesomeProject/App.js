bce3221e9097999dfa0c4a86631aa7d1118e069c
林采如 <lintsaiju@lincairudeMacBook-Pro.local>
2022-09-26 20:28:20 -0700

    ver1 to be fixed

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,22 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,TextInput} from 'react-native';
+import { StyleSheet, Text, View ,TextInput,Button} from 'react-native';
 import Header from './components/Header.js';
 import React,{useState} from 'react';
 import Input from './components/Input.js';
 
 export default function App() {
+const [modalVisible,setModalVisible]=useState(false);
+
 const onTextAdd=function(newText){
   console.log(newText);
+  setModalVisible(false);
 }
 
   const name="my app";
   const [text,setText]=useState("");
+  const makeModalVisible=()=>{setModalVisible(true)}
+  const makeModalInvisible=()=>{setModalVisible(false)}
   return (
     <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on {name}!</Text> */}
-      <Header appName={name}/>
-      <Input onAdd={onTextAdd}/>
+      <View style={styles.topContainer}></View>
+      <Text>Open up App.js to start working on {name}!</Text>
+      {/* <Header appName={name}/> */}
+      
+      <Button title="add a goal" onPress={makeModalVisible}/>
+      <Input modal={modalVisible} onAdd={onTextAdd} onCancel={makeModalInvisible}/>
+      <View style={styles.bottomContainer}>
+        <View style={styles.textContainer}>
       <Text>inputed:{text}</Text>
+      </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -24,9 +36,21 @@ const onTextAdd=function(newText){
 
 const styles = StyleSheet.create({
   container: {
+    
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+    // flexDirection:'row'
   },
+  topContainer:{
+flex:1,
+alignItems: 'center',
+justifyContent: 'center',
+  },
+  bottomContainer:{
+    flex:4,
+    backgroundColor:'pink',
+    alignItems: 'center',
+  }
 });
--- AwesomeProject/components/Input.js
+++ AwesomeProject/components/Input.js
@@ -1,32 +1,41 @@
-import { StyleSheet, Text, View ,TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View ,TextInput, Button,Modal,Image} from 'react-native';
 import React,{useState} from 'react';
 
-export default function Input({onAdd}) {
+export default function Input({onAdd,modal,onCancel}) {
     
     const [text,setText]=useState("");
     return (
+      <Modal visible={modal}>
       <View style={styles.container}>
-
+        <Image 
+        
+        source={{uri:'https://cdn-icons-png.flaticon.com/512/2617/2617812.png'}}
+        style={styles.image}
+        />
+      <TextInput 
+        value={text}
+        onChangeText={(newText)=>{setText(newText)}}
+        placeholder="type sth"
+        style={styles.input}
+        />
+        <View style={styles.button,styles.buttonContainer}>
         <Button
         title="confirm"
         onPress={()=>{
             onAdd(text);
             setText("");
         }}
+        disabled={text.length?false:true}
         // if onAdd not in a function, will render repeatedly
         />
         
-        <TextInput 
-        value={text}
-        onChangeText={(newText)=>{setText(newText)}}
-        placeHolder="type sth"
-        />
-        
+        <Button title="cancel" onPress={onCancel}/>
+        </View>
         <Text>inputed:{text}</Text>
       </View>
-    );
-  }
-  
+      </Modal>
+  );
+}
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -34,5 +43,23 @@ export default function Input({onAdd}) {
       alignItems: 'center',
       justifyContent: 'center',
     },
+    input:{
+      color:'red',
+      borderBottomWidth:2,
+      borderBottomColor:'purple',
+      margin:5,
+      padding:10
+    },
+    button:{
+      margin:5,
+      width:'30%'
+    },
+    image:{
+      width:100,
+      height:100
+    },
+    buttonContainer:{
+flexDirection:'row'
+    }
   });
   
