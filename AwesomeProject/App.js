b545c2214d3c099eb66d17b619a1f2a1f32c3d37
林采如 <lintsaiju@lincairudeMacBook-Pro.local>
2022-09-29 17:27:07 -0700

    wk3 add container style

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,56 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,TextInput,Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, SafeAreaView } from 'react-native';
 import Header from './components/Header.js';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Input from './components/Input.js';
 
 export default function App() {
-const [modalVisible,setModalVisible]=useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
 
-const onTextAdd=function(newText){
-  console.log(newText);
-  setModalVisible(false);
-}
+  const onTextAdd = function (newText) {
+    console.log(newText);
+    setModalVisible(false);
+  }
 
-  const name="my app";
-  const [text,setText]=useState("");
-  const makeModalVisible=()=>{setModalVisible(true)}
-  const makeModalInvisible=()=>{setModalVisible(false)}
+  const name = "my app";
+  const [text, setText] = useState("");
+  const makeModalVisible = () => { setModalVisible(true) }
+  const makeModalInvisible = () => { setModalVisible(false) }
   return (
-    <View style={styles.container}>
-      <View style={styles.topContainer}></View>
-      <Text>Open up App.js to start working on {name}!</Text>
-      {/* <Header appName={name}/> */}
-      
-      <Button title="add a goal" onPress={makeModalVisible}/>
-      <Input modal={modalVisible} onAdd={onTextAdd} onCancel={makeModalInvisible}/>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
+        {/* <Text>Open up App.js to start working on {name}!</Text> */}
+        <Header appName={name} />
+        <Button title="add a goal" onPress={makeModalVisible} />
+      </View>
+
       <View style={styles.bottomContainer}>
         <View style={styles.textContainer}>
-      <Text>inputed:{text}</Text>
+          <Text style={styles.text}>you typed...{text}</Text>
+        </View>
       </View>
-      </View>
+      <Input modal={modalVisible} onAdd={onTextAdd} onCancel={makeModalInvisible} />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+
     flex: 1,
     backgroundColor: '#fff',
     // alignItems: 'center',
     justifyContent: 'center',
     // flexDirection:'row'
   },
-  topContainer:{
-flex:1,
-alignItems: 'center',
-justifyContent: 'center',
-  },
-  bottomContainer:{
-    flex:4,
-    backgroundColor:'pink',
+  topContainer: {
+    flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomContainer: {
+    flex: 4,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+  },
+  textContainer: {
+    backgroundColor: "#aaa",
+    borderRadius: 5,
+    color: 'blue'
+  },
+  text: {
+
+
   }
 });
--- AwesomeProject/components/Header.js
+++ AwesomeProject/components/Header.js
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 // export default function Header(props) {
-  export default function Header(appName) {
+  export default function Header({appName}) {
   return (
     <View>
       {/* <Text>Open up App.js to start working on {props.name}!</Text> */}
