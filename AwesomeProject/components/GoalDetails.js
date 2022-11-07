bca32791b5c9c2233e0db4249988c29b6a6b40c1
林采如 <lintsaiju@lincairudeMacBook-Pro.local>
2022-11-07 11:28:12 -0800

    week8

--- AwesomeProject/components/GoalDetails.js
+++ AwesomeProject/components/GoalDetails.js
@@ -1,11 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Button } from 'react-native'
+import React,{useEffect,useState} from 'react'
 
 export default function GoalDetails({route, navigation}) {
-    console.log(route)
+  const [users,setUsers]=useState([]);
+
+  useEffect(()=>{
+    const fetchUsers=async()=>{
+      try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok){
+    throw new Error('the fetch request failed')  
+  }
+  const data=await response.json();
+  console.log('read',data);
+  setUsers(data);
+  }
+  catch (err){
+console.log(err)
+  }}
+    fetchUsers();
+  },[])
+
+  const addUser=async()=>{
+    try{
+    const result=await fetch('https://jsonplaceholder.typicode.com/users',{
+method:'POST',
+headers:{
+  'Content-type':'application/json'
+},
+//convert JS object to JSON
+body:JSON.stringify(
+  {name:'Neda'}
+  )
+    });
+    if (!response.ok){
+      throw new Error('post fetch failed')
+    }
+    const data=await response.json();
+    console.log('add',data);
+    setUsers(prevUsers=>[prevUsers,data]);
+  }catch(err){
+    console.log(err)}
+  };
+
+    // console.log(route)
     return (
     <View>
       <Text>GoalDetails:{route.params.goalObject.text}</Text>
+      <Button title='addUser' onPress={addUser}/>
+      <FlatList data={users} renderItem={({item})=>{
+        console.log('item',item)
+        return <Text>{item.name}</Text>
+
+      }}/>
     </View>
   )
 }
--- AwesomeProject/components/Header.js
+++ AwesomeProject/components/Header.js
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,StyleSheet,Dimensions,useWindowDimensions, Platform } from 'react-native'
+import React from 'react';
+
+//not update when rotating
+const windowWidth=Dimensions.get('window').width;
+const windowHeight=Dimensions.get('window').height;
 
 // export default function Header(props) {
   export default function Header({appName}) {
+    //updatw when orientation changes
+    const {width,height}=useWindowDimensions();
+    const paddingVerticalDyn=height<415? 0:10;//landscape:0
   return (
-    <View>
+
+    <View style={styles.headerContainer}>
       {/* <Text>Open up App.js to start working on {props.name}!</Text> */}
-      <Text>Open up App.js to start working on {appName}!</Text>
+      <Text style={[styles.headerText,{paddingVertical:paddingVerticalDyn}]}>welcome to {appName}!</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  headerContainer:{
+    justifyContent:'center',
+    // alignItems:'center',
+    width:350,
+    maxWdith:'90%'
+  },
+  headerText:{
+    color:'rebeccapurple',
+    fontWeight:'bold',
+    borderStyle:'dotted',
+    borderColor:'slateblue',
+    // borderWidth:2,
+    borderWidth:Platform.OS==='android'?4:0,
+    // borderWidth:Platform.select({ios:0,android:4}),
+    fontsize:windowWidth<380? 18:22,
+    paddingHoriziontal:windowWidth<380? 10: 20,
+    paddingVertical:10,
+    borderRadius:5,
+    textAlign:'center'
+
+  }
+})
